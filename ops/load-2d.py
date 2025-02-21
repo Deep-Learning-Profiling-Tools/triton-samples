171e358b4e3dc07c626d1fdb1c6c0b54978b75f8
Jokeren <robinho364@gmail.com>
2025-02-20 21:01:14 -0500

    Update

--- ops/load-2d.py
+++ ops/load-2d.py
@@ -12,25 +12,32 @@ def kernel(
     STRIDE: tl.constexpr,
     CHANNEL_SIZE: tl.constexpr,
     BLOCK_SIZE: tl.constexpr,
+    C0: tl.constexpr,
+    C1: tl.constexpr,
 ):
     pid = tl.program_id(0)
     offsets = (
         tl.arange(0, BLOCK_SIZE)[:, None] * STRIDE + tl.arange(0, CHANNEL_SIZE)[None, :]
     )
+    offsets = tl.max_contiguous(offsets, (C0, C1))
     x_val = tl.load(x_ptr + offsets + pid * BLOCK_SIZE * CHANNEL_SIZE)
     tl.store(y_ptr + offsets + pid * BLOCK_SIZE * CHANNEL_SIZE, x_val)
 
 
 BATCH_SIZE = 512
 BLOCK_SIZE = 64
+CHANNEL_SIZE = 64
 
 for dtype in [torch.float16, torch.float32]:
-    for c in [1, 2, 4, 8, 16]:
-        x = torch.randn((BATCH_SIZE, BLOCK_SIZE, c), device="cuda")
-        y = torch.zeros_like(x)
-        with proton.scope(
-            f"{dtype}_{c}", {"bytes": x.nelement() * x.element_size() * 2}
-        ):
-            kernel[(BATCH_SIZE,)](x, y, c, c, BLOCK_SIZE)
+    for c0 in [1, 2, 4, 8]:
+        for c1 in [1, 2, 4, 8]:
+            x = torch.randn((BATCH_SIZE, BLOCK_SIZE, CHANNEL_SIZE), device="cuda")
+            y = torch.zeros_like(x)
+            with proton.scope(
+                f"{dtype}_{c0}_{c1}", {"bytes": x.nelement() * x.element_size() * 2}
+            ):
+                kernel[(BATCH_SIZE,)](
+                    x, y, CHANNEL_SIZE, CHANNEL_SIZE, BLOCK_SIZE, c0, c1
+                )
 
-        torch.testing.assert_close(x, y, rtol=1e-2, atol=1e-2)
+            torch.testing.assert_close(x, y, rtol=1e-2, atol=1e-2)
